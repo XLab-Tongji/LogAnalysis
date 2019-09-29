572c0342a5d737ee376332c78e016635ae933df5
Liren He <hlr7999@gmail.com>
2019-09-30 01:13:11 +0800

    file input

--- valueExtract/valueExtract.py
+++ valueExtract/valueExtract.py
@@ -18,26 +18,31 @@ def valueExtract(pattern, logs, tool=0, ouput="result.txt"):
         last_is_pattern = False
         # 遍历模式字符串进行匹配
         for pattern_str in pattern_arr:
+            # 如果是value
             if pattern_str[0] == start_char and pattern_str[-1] == start_char:
                 if pattern_str[1:-1] == "msgtime":
                     cur_log_str += (" " + log_arr[log_index+1] + " " + log_arr[log_index+2])
                     log_index += 2
                 elif pattern_str[1:-1] == "time":
                     # To Do
+                    # time 情况太多 目前无法识别
                     pass
                 log_value.append(cur_log_str)
                 log_index += 1
                 if (log_index < len(log_arr)):
                     cur_log_str = log_arr[log_index]
                 last_is_pattern = True
+            # 如果是匹配的单词
             elif cur_log_str.lower() == pattern_str.lower():
                 log_index += 1
                 if (log_index < len(log_arr)):
                     cur_log_str = log_arr[log_index]
                 last_is_pattern = False
+            # 如果是单词前一部分匹配
             elif len(cur_log_str) >= len(pattern_str) and cur_log_str.lower()[0:len(pattern_str)] == pattern_str.lower():
                 cur_log_str = cur_log_str[len(pattern_str):]
                 last_is_pattern = False
+            # 此时在前一个字符串中, 如果前一个字符串是value则需要重新取值
             elif last_is_pattern:
                 log_index -= 1
                 cur_log_str = log_value.pop()
@@ -57,24 +62,32 @@ def valueExtract(pattern, logs, tool=0, ouput="result.txt"):
         f.writelines(lines)
     return values
 
+'''
+向量化目前还没做，主要是不确定我的理解是否正确。
+先记录一下我的理解
+举例来说，如果得到的value数组如下
+[t1, v11, v21]
+[t2, v12, v22]
+[t3, v13, v23]
+......
+[tn, v1n, v2n]
+
+我的理解是，首先把时间变为t1-t0，t2-t1这种形式
+然后把每个value都进行标准化（即将每一列标准化）（应该只针对定量的数据）
+[t1-t0, v11', v21']
+[t2-t1, v12', v22']
+......
+[tn-t(n-1), v1n', v2n']
+然后定一个时间步长，每个time step的输入是上面的一个向量（相当于有多个feature）
+也就是LSTM输入的x1...xt中每个x都是一个向量（拥有多个feature）
+'''
+
 if __name__ == "__main__":
-    pattern = r"%msgtime% combo %string% ( pam_unix ) [ %integer% ] : %action% %status% ; logname = uid = %srcuid% euid = %integer% tty = %string% ruser = rhost = %srcuser%"
-    logs = [
-        "Jun 10 17:16:34 combo sshd(pam_unix)[3353]: authentication failure; logname= uid=0 euid=0 tty=NODEVssh ruser= rhost=itsc.iasi.astral.ro",
-        "Jun 13 20:13:11 combo sshd(pam_unix)[17422]: authentication failure; logname= uid=0 euid=0 tty=NODEVssh ruser= rhost=under-eepc58.kaist.ac.kr",
-        "Jun 13 20:13:11 combo sshd(pam_unix)[17432]: authentication failure; logname= uid=0 euid=0 tty=NODEVssh ruser= rhost=under-eepc58.kaist.ac.kr",
-        "Jun 13 20:13:11 combo sshd(pam_unix)[17421]: authentication failure; logname= uid=0 euid=0 tty=NODEVssh ruser= rhost=under-eepc58.kaist.ac.kr",
-        "Jun 13 20:13:11 combo sshd(pam_unix)[17423]: authentication failure; logname= uid=0 euid=0 tty=NODEVssh ruser= rhost=under-eepc58.kaist.ac.kr",
-        "Jun 13 20:13:11 combo sshd(pam_unix)[17428]: authentication failure; logname= uid=0 euid=0 tty=NODEVssh ruser= rhost=under-eepc58.kaist.ac.kr",
-        "Jun 13 20:13:11 combo sshd(pam_unix)[17420]: authentication failure; logname= uid=0 euid=0 tty=NODEVssh ruser= rhost=under-eepc58.kaist.ac.kr",
-        "Jun 13 20:13:11 combo sshd(pam_unix)[17427]: authentication failure; logname= uid=0 euid=0 tty=NODEVssh ruser= rhost=under-eepc58.kaist.ac.kr",
-        "Jun 13 20:13:11 combo sshd(pam_unix)[17431]: authentication failure; logname= uid=0 euid=0 tty=NODEVssh ruser= rhost=under-eepc58.kaist.ac.kr",
-        "Jun 13 20:13:11 combo sshd(pam_unix)[17433]: authentication failure; logname= uid=0 euid=0 tty=NODEVssh ruser= rhost=under-eepc58.kaist.ac.kr",
-        "Jun 13 20:13:11 combo sshd(pam_unix)[17429]: authentication failure; logname= uid=0 euid=0 tty=NODEVssh ruser= rhost=under-eepc58.kaist.ac.kr",
-        "Jun 15 14:53:32 combo sshd(pam_unix)[23661]: authentication failure; logname= uid=0 euid=0 tty=NODEVssh ruser= rhost=061092085098.ctinets.com", 
-        "Jun 15 14:53:32 combo sshd(pam_unix)[23663]: authentication failure; logname= uid=0 euid=0 tty=NODEVssh ruser= rhost=061092085098.ctinets.com",
-        "Jun 15 14:53:32 combo sshd(pam_unix)[23664]: authentication failure; logname= uid=0 euid=0 tty=NODEVssh ruser= rhost=061092085098.ctinets.com",
-        "Jun 15 14:53:33 combo sshd(pam_unix)[23665]: authentication failure; logname= uid=0 euid=0 tty=NODEVssh ruser= rhost=061092085098.ctinets.com",
-    ]
+    logs = []
+    with open("input.txt") as f:
+        for line in f:
+            logs.append(line)
+    pattern = logs[0]
+    logs = logs[1:]
     valueExtract(pattern, logs)
     print("=====done=====")
