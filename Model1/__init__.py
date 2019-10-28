6e572ae3ca0f4afa5fb7ce0638fc5691a87522dd
shuangliz <358355029@qq.com>
2019-10-28 10:28:37 +0800

    add a file to record the parameters of model1

---
+++ Model1/__init__.py
@@ -0,0 +1,12 @@
+# !/usr/bin/python
+# -*- coding:utf-8 -*-
+
+
+# parameters for model1
+window_size = 6
+input_size = 1
+hidden_size = 20
+num_layers = 3
+num_classes = 50  # len(pattern2log)+1
+model_dir = 'output/model'
+log_dir = 'output/log'
