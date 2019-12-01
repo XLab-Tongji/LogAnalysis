5912fc2d9001e363d3936394e576e54206fd0625
logan <807654909@qq.com>
2019-12-01 18:32:38 +0800

    modify value

--- k8s-2/valuedeal.py
+++ k8s-2/valuedeal.py
@@ -6,13 +6,14 @@ with open("value.log","r")as valuedeal:
 
 dealvalue=[]
 for i in range(len(values)-1):
-    v1=values[i][:-1].split(" ")
-    v2=values[i+1][:-1].split(" ")
-    time1=datetime(int(v1[0]),int(v1[1]),int(v1[2]),hour=int(v1[3]),minute=int(v1[4]),second=int(v1[5]),microsecond=int(v1[6]))
-    time2=datetime(int(v2[0]),int(v2[1]),int(v2[2]),hour=int(v2[3]),minute=int(v2[4]),second=int(v2[5]),microsecond=int(v2[6]))
-    time=(time2.day-time1.day)*86400000+(time2.hour-time1.hour)*3600000+(time2.minute-time1.minute)*60000+(time2.second-time1.second)*1000+(time2.microsecond-time1.microsecond)
-    v=str(time)+" "+v2[7]+" "+v2[8]+" "+v2[9]+"\n"
-    dealvalue.append(v)
+    if(i!=2000 and i!=2500 and i !=3000):   #把两个模块之间的那个时间差不计算
+        v1=values[i][:-1].split(" ")
+        v2=values[i+1][:-1].split(" ")
+        time1=datetime(int(v1[0]),int(v1[1]),int(v1[2]),hour=int(v1[3]),minute=int(v1[4]),second=int(v1[5]),microsecond=int(v1[6]))
+        time2=datetime(int(v2[0]),int(v2[1]),int(v2[2]),hour=int(v2[3]),minute=int(v2[4]),second=int(v2[5]),microsecond=int(v2[6]))
+        time=(time2.day-time1.day)*86400000+(time2.hour-time1.hour)*3600000+(time2.minute-time1.minute)*60000+(time2.second-time1.second)*1000+(time2.microsecond-time1.microsecond)
+        v=str(time)+" "+v2[7]+" "+v2[8]+" "+v2[9]+"\n"
+        dealvalue.append(v)
 
 with open("dealedvalue.log","a+")as dvalue:
     for i in dealvalue:
--- k8s-2/valueextract.py
+++ k8s-2/valueextract.py
@@ -18,22 +18,24 @@ for i in file_names:
                 j=int(j)
                 with open("normalize_value.log", 'r')as value:
                     values = value.readlines()
-                    if(j-1<len(values)):
-                        text = values[j - 1]
+                    if(j!=1 and j!=2001 and j!=2501 and j!=3001):
+                        if(j<=2000):
+                            text = values[j - 1-1]
+                            #写到logvalue_train lide 1.log li
+                            with open(RootPath+'logvalue/logvalue_train/'+i,'a+')as t:
+                                    t.write(text)
+                        elif(j<2500):
+                            text = values[j - 2-1]
+                            with open(RootPath+'logvalue/logvalue_val/'+i,'a+')as t:
+                                    t.write(text)
+                        elif(j<3000):
+                            text = values[j - 3-1]
+                            with open(RootPath + 'logvalue/logvalue_test/' + i, 'a+')as t:
+                                t.write(text)
+                        else:
+                            text = values[j - 4-1]
+                            with open(RootPath + 'logvalue/logvalue_abnormal/' + i, 'a+')as t:
+                                t.write(text)
                     else:
-                        text=""
-                if(j<=2000):
-                    #写到logvalue_train lide 1.log li
-                        with open(RootPath+'logvalue/logvalue_train/'+i,'a+')as t:
-                            t.write(text)
-                elif(j<2500):
-                    with open(RootPath+'logvalue/logvalue_val/'+i,'a+')as t:
-                            t.write(text)
-                elif(j<3000):
-                    with open(RootPath + 'logvalue/logvalue_test/' + i, 'a+')as t:
-                        t.write(text)
-                else:
-                    with open(RootPath + 'logvalue/logvalue_abnormal/' + i, 'a+')as t:
-                        t.write(text)
-
+                        continue
 
