a564b95b107284ec493ddda3918a5e03b63a61a1
logan <807654909@qq.com>
2019-12-01 15:23:11 +0800

    k8s value modify

--- k8s-2/valuedeal.py
+++ k8s-2/valuedeal.py
@@ -9,10 +9,8 @@ for i in range(len(values)-1):
     v2=values[i+1][:-1].split(" ")
     time1=datetime(int(v1[0]),int(v1[1]),int(v1[2]),hour=int(v1[3]),minute=int(v1[4]),second=int(v1[5]),microsecond=int(v1[6]))
     time2=datetime(int(v2[0]),int(v2[1]),int(v2[2]),hour=int(v2[3]),minute=int(v2[4]),second=int(v2[5]),microsecond=int(v2[6]))
-    timedays=(time2-time1).days
-    timeseconds=(time2-time1).seconds
-    timemsonds=(time2-time1).microseconds
-    v=str(timedays)+" "+str(timeseconds)+" "+str(timemsonds)+" "+v1[7]+" "+v1[8]+" "+v1[9]+"\n"
+    time=(time2-time1).days*86400000+(time2-time1).seconds*1000+(time2-time1).microseconds
+    v=str(time)+" "+v1[7]+" "+v1[8]+" "+v1[9]+"\n"
     dealvalue.append(v)
 
 with open("dealedvalue.log","a+")as dvalue:
